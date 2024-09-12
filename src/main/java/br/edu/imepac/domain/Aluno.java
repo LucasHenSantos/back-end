f9c80aaec418b7e39525a8062eeca00e2385dcde
LucasHenSantos <Lucas.henrique@aluno.imepac.edu.br>
2024-09-11 22:21:45 -0300

    ApplicationRun

--- src/main/java/br/edu/imepac/domain/Aluno.java
+++ src/main/java/br/edu/imepac/domain/Aluno.java
@@ -2,10 +2,40 @@ package br.edu.imepac.domain;
 
 public class Aluno {
 
+
     private String nome;
 
     private String matricula;
 
     private String email;
 
+    public Aluno(String nome, String matricula, String email) {
+        this.nome = nome;
+        this.matricula = matricula;
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
 }
